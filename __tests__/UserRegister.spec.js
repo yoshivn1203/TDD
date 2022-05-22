f7fcd7a6344c10d563423aaa104f845b2ea41478
yoshivn1203 <yoshivn1203@gmail.com>
2022-05-22 11:34:36 +0700

    database

--- __tests__/UserRegister.spec.js
+++ __tests__/UserRegister.spec.js
@@ -1,6 +1,14 @@
 const request = require('supertest');
-const { response } = require('../src/app');
 const app = require('../src/app');
+const User = require('../src/user/User');
+const sequelize = require('../src/config/database');
+
+beforeAll(() => {
+  return sequelize.sync();
+});
+beforeEach(() => {
+  return User.destroy({ truncate: true });
+});
 
 describe('User Registration', () => {
   it('return 200 Ok when sign up request is valid', done => {
@@ -28,6 +36,41 @@ describe('User Registration', () => {
       .then(response => {
         expect(response.body.message).toBe('User created');
         done();
+      });
+  });
+
+  it('save user to database', done => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword'
+      })
+      .then(() => {
+        User.findAll().then(userList => {
+          expect(userList.length).toBe(1);
+          done();
+        });
+      });
+  });
+
+  it('save username and email to database', done => {
+    request(app)
+      .post('/api/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@mail.com',
+        password: 'P4ssword'
+      })
+      .then(() => {
+        User.findAll().then(userList => {
+          const savedUser = userList[0];
+
+          expect(savedUser.username).toBe('user1');
+          expect(savedUser.email).toBe('user1@mail.com');
+          done();
+        });
       });
   });
 });
